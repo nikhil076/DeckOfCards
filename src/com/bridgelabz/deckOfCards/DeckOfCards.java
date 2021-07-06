f2d456db7e49f782e32dbb9ef07f33d1354bb7b5
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-06 23:22:05 +0530

    Initialized the program to create and distribute deck of cards for 4 players

---
+++ src/com/bridgelabz/deckOfCards/DeckOfCards.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.deckOfCards;
+
+public class DeckOfCards {
+	String[] SUITS = {
+	        "Clubs", "Diamonds", "Hearts", "Spades"
+	    };
+
+	    String[] RANKS = {
+	        "2", "3", "4", "5", "6", "7", "8", "9", "10",
+	        "Jack", "Queen", "King", "Ace"
+	    };
+	    int n = SUITS.length * RANKS.length;
+	    String[] deck = new String[n];
+	    // initialize deck
+	    void deck() 
+	    {
+		    for (int i = 0; i < RANKS.length; i++) {
+		        for (int j = 0; j < SUITS.length; j++) {
+		            deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+		        }
+		    }
+	    }
+	    // shuffle
+	    void shuffle()
+	    {
+		    for (int i = 0; i < n; i++) {
+		        int r = i + (int) (Math.random() * (n-i));
+		        String temp = deck[r];
+		        deck[r] = deck[i];
+		        deck[i] = temp;
+		    }
+	    }
+	    // print shuffled deck
+	    void shuffleDeckForPlayers()
+	    {
+		    for (int i = 0; i < 4; i++) {
+		        System.out.println("\n Person " + (i + 1));
+		        for (int j = 0; j < 4; j++) {
+		            System.out.println(deck[i + j * 4]);
+		        }
+		    }
+	    }
+	public static void main(String[] args)
+	{
+		DeckOfCards deckOfCards = new DeckOfCards();
+		deckOfCards.deck();
+		deckOfCards.shuffle();
+		deckOfCards.shuffleDeckForPlayers();
+	}
+}
